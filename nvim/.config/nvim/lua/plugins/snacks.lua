808a503561043fbcfcb1a15a708cd6d9d991c06e
NOTHING-R <kalilinux.nothing@gmail.com>
2026-07-08 09:52:49 +0600

    Changed some issue and added org agenda and dbui

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = { enabled = false },
+    -- notifier = { enabled = false },
 
     indent = {
       enabled = true, -- ← This turns on the vertical indent guides permanently
